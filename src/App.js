f6efe403c046a6b3588e26cf8cc0d63deb52ede4
Hussein-shahatet97 <hussein.sh1997@hotmail.com>
2020-03-26 14:41:55 +0200

    Delete Task #7

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import AddTodo from './component/AddTodo';
 import TitleTodo from './component/TitleTodo'
 import TodoList from './component/TodoList'
+import DeleteTask from './component/DeleteTask'
+
 import './App.css';
-import {library} from '@fortawesome/fontawesome-svg-core'
-import {faTrash} from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faTrash);
-class App extends Component{
-  state={
-    todos:[ ]
- };
+class App extends Component {
+  state = {
+    todos: []
+  };
 
- addTodoI = (todo) =>{
-     this.setState({
-         todos:[{text:todo,
-          completed:false
-        },...this.state.todos]
-     });
+  addTodoI = (todo) => {
+    this.setState({
+      todos: [{
+        text: todo,
+        completed: false
+      }, ...this.state.todos]
+    });
     console.log(todo);
-    
 
- };
-  render(){
-  return (
-    <div className="App">
-      <TitleTodo/>
-      <AddTodo onSubmit={this.addTodoI}/>
-      <TodoList todos={this.state.todos}/>
-      
-    </div>
-  );
-  // componentDidMount=()=>{
 
-  // }
-}
+  };
+
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter(item =>
+      item.key !== key);
+    this.setState({
+      items: filteredItems
+    })
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <TitleTodo />
+        <AddTodo onSubmit={this.addTodoI} />
+        <TodoList todos={this.state.todos} />
+        <DeleteTask onSubmit={this.deleteItem} />
+
+      </div>
+    );
+    // componentDidMount=()=>{
+
+    // }
+  }
 }
 
 export default App;
